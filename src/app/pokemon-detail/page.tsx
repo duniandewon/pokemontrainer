e75efc635d86e871a2e69f2bbbcaf0f330618fee
ndewon <mohammad_baragbah@telkomsel.co.id>
2024-02-09 22:46:49 +0700

    "Fix layout"

--- src/app/pokemon-detail/page.tsx
+++ src/app/pokemon-detail/page.tsx
@@ -6,12 +6,12 @@ import { Pokemon } from "./_components/pokemon";
 export default function PokemonDetail() {
   return (
     <div className="h-full w-full grid grid-rows-[1fr_auto] gap-5 p-5">
-      <footer>
-        <Berries />
-      </footer>
       <main>
         <Pokemon />
       </main>
+      <footer>
+        <Berries />
+      </footer>
     </div>
   );
 }
