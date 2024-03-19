e12dcda033355b6fafb39fc5838956089673a7b9
ndewon <mohammad_baragbah@telkomsel.co.id>
2024-03-20 02:11:01 +0700

    revert: remove virtualized list

--- src/app/(pokemons)/_components/PokemonItem.tsx
+++ src/app/(pokemons)/_components/PokemonItem.tsx
@@ -9,8 +9,8 @@ interface Props {
 export function PokemonItem({ pokemon, isSelected }: Props) {
   return (
     <div
-      className={`cursor-pointer border hover:border-slate-300 p-2 ${
-        isSelected ? "border-slate-300" : "border-transparent"
+      className={`cursor-pointer border p-2 ${
+        isSelected ? "border-slate-300" : "border-transparent hover:border-slate-300"
       }`}
     >
       <Image src={pokemon.image} alt={pokemon.name} width={150} height={150} />
