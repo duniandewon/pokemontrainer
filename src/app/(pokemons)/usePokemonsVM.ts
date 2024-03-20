f70527e7189c7e4462d7333f5f9b1850c7702758
ndewon <mohammad_baragbah@telkomsel.co.id>
2024-03-20 14:37:26 +0700

    fix: redirect to pokemon-detail if already chose a pokemon

--- src/app/(pokemons)/usePokemonsVM.ts
+++ src/app/(pokemons)/usePokemonsVM.ts
@@ -1,12 +1,16 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import { useGetPokemons } from "./_hooks/useGetPokemons";
 import { useSearch } from "./_hooks/useSearch";
 import { useChoosePokemon } from "./_hooks/useChoosePokemon";
 import { useGetPokemonDetail } from "@/hooks/useGetPokemonDetail";
+import { useGetMyPokemon } from "@/hooks/useGetMyPokemon";
+import { useRouter } from "next/navigation";
 
 export function usePokemonsVM() {
   const limit = useRef(40);
+
+  const router = useRouter();
 
   const [selectedPokemon, setSelectedPokemon] = useState(0);
 
@@ -30,6 +34,14 @@ export function usePokemonsVM() {
       onChoosePokemon(data.data);
     }
   };
+
+  const { data } = useGetMyPokemon();
+
+  useEffect(() => {
+    if (data) {
+      router.push("/pokemon-detail");
+    }
+  }, [data, router]);
 
   return {
     pokemons,
