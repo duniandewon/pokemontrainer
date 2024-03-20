d33c20b8cbebb12b64f0e0d23086417870b733ee
ndewon <mohammad_baragbah@telkomsel.co.id>
2024-03-20 14:18:39 +0700

    fix: check if pokemon has image

--- src/app/(pokemons)/_components/PokemonItem.tsx
+++ src/app/(pokemons)/_components/PokemonItem.tsx
@@ -9,11 +9,24 @@ interface Props {
 export function PokemonItem({ pokemon, isSelected }: Props) {
   return (
     <div
-      className={`cursor-pointer border p-2 ${
-        isSelected ? "border-slate-300" : "border-transparent hover:border-slate-300"
+      className={`flex items-center h-full w-full justify-center cursor-pointer border p-2 ${
+        isSelected
+          ? "border-slate-300"
+          : "border-transparent hover:border-slate-300"
       }`}
     >
-      <Image src={pokemon.image} alt={pokemon.name} width={150} height={150} />
+      {pokemon.image ? (
+        <Image
+          src={pokemon.image}
+          alt={pokemon.name}
+          width={150}
+          height={150}
+        />
+      ) : (
+        <h2 className="text-sm text-center">
+          {pokemon.name} image place holder
+        </h2>
+      )}
     </div>
   );
 }
