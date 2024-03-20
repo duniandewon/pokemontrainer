5d856160b4a35f97b85f9c83389dba427ebbf9c9
ndewon <mohammad_baragbah@telkomsel.co.id>
2024-03-20 21:48:11 +0700

    fix: use useMutation to update localstorage when selecting a pokemon

--- src/app/(pokemons)/_hooks/useChoosePokemon.ts
+++ src/app/(pokemons)/_hooks/useChoosePokemon.ts
@@ -1,10 +1,45 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
 import { choosePokemonUseCase } from "@/Domain/pokemon/UseCase/choosePokemon.usecase";
 
 import { PokemonDetail } from "@/Domain/pokemon/Model/PokemonDetail";
 
 export function useChoosePokemon(choosePokemonUC = choosePokemonUseCase()) {
+  const queryClient = useQueryClient();
+
+  const { mutate } = useMutation({
+    mutationFn: async (pokemon: PokemonDetail) => {
+      choosePokemonUC.invoke(pokemon);
+    },
+    onMutate: async (pokemon) => {
+      await queryClient.cancelQueries({ queryKey: ["my-pokemon"] });
+
+      const prevPokemon = queryClient.getQueryData<PokemonDetail>([
+        "my-pokemon",
+      ]);
+
+      console.log("pokemon: ", pokemon);
+
+      if (prevPokemon) {
+        queryClient.setQueryData(["my-pokemon"], pokemon);
+      }
+
+      return { prevPokemon };
+    },
+    onError: (err, variables, context) => {
+      if (context?.prevPokemon)
+        queryClient.setQueryData<PokemonDetail>(
+          ["my-pokemon"],
+          context.prevPokemon
+        );
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: ["my-pokemon"] });
+    },
+  });
+
   const onChoosePokemon = (pokemon: PokemonDetail) => {
-    choosePokemonUC.invoke(pokemon);
+    mutate(pokemon);
   };
 
   return {
